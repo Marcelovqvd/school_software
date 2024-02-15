16a29588d29b7403a4ae9e7f2ac1b5f77b86e045
Marcelo <marcelovqvd@gmail.com>
2024-02-15 17:00:44 -0300

    feat: React Helmet (SEO)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,18 @@
 import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+import "./global.css";
 
 import { router } from "./routes";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
 
 export default App;
